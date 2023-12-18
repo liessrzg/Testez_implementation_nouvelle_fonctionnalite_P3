19517e42cbdc5a16f608c6f44ac77d4937b14815
REZEG Liess <liess.rezeg@gmail.com>
2023-12-18 18:08:03 +0100

    Test unitaire et d'intégration

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,105 @@
-﻿using Xunit;
+﻿using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using Xunit;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+ 
+
+
+        public class ProductViewModelValidationTest
         {
-            // Arrange
+            private ProductViewModel product;
 
-            // Act
+            public ProductViewModelValidationTest()
+            {
+                product = new ProductViewModel();
+            }
+
+            [Fact]
+            public void TestMissingPrice()
+            {
+                product.Name = "Test Product";
+                product.Stock = 1;
+                product.Price = null;
+                Assert.False(ValidateModel(product));
+                Assert.Equal("MissingPrice", GetFirstErrorMessage(product));
+            }
+
+            [Fact]
+            public void TestMissingName()
+            {
+                product.Name = null;
+                product.Price = 10.0;
+                product.Stock = 1;
+                Assert.False(ValidateModel(product));
+                Assert.Equal("MissingName", GetFirstErrorMessage(product));
+            }
+
+            [Fact]
+            public void TestPriceNotANumber()
+            {
+                product.Name = "Test Product";
+                product.Price = double.NaN;// Not a Number
+                product.Stock = 1;
+                Assert.False(ValidateModel(product));
+                Assert.Equal("PriceNotANumber", GetFirstErrorMessage(product));
+            }
+
+            [Fact]
+            public void TestPriceNotGreaterThanZero()
+            {
+                product.Name = "Test Product";
+                product.Price = 0.0;
+                product.Stock = 1;
+                Assert.False(ValidateModel(product));
+                Assert.Equal("PriceNotGreaterThanZero", GetFirstErrorMessage(product));
+            }
 
 
-            // Assert
-            Assert.Equal(1, 1);
+
+            [Fact]
+            public void TestQuantityNotGreaterThanZero()
+            {
+                product.Name = "Test Product";
+                product.Price = 10.0;
+                product.Stock = 0;
+                Assert.False(ValidateModel(product));
+                Assert.Equal("QuantityNotGreaterThanZero", GetFirstErrorMessage(product));
+            }
+
+
+
+            // Ajoutez ici d'autres méthodes pour tester les autres cas...
+
+            private bool ValidateModel(object model)
+            {
+                var validationResults = new List<ValidationResult>();
+                var ctx = new ValidationContext(model, null, null);
+                return Validator.TryValidateObject(model, ctx, validationResults, true);
+            }
+
+            private string GetFirstErrorMessage(object model)
+            {
+                var validationResults = new List<ValidationResult>();
+                var ctx = new ValidationContext(model, null, null);
+                Validator.TryValidateObject(model, ctx, validationResults, true);
+                return validationResults[0].ErrorMessage;
+            }
         }
 
-        // TODO write test methods to ensure a correct coverage of all possibilities
+
+
+        // TODO écrire des méthodes de test pour assurer une couverture correcte de toutes les possibilités
     }
+
 }
---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/TestIntegration.cs
@@ -0,0 +1,106 @@
+﻿// ... (importations)
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Controllers;
+using P3AddNewFunctionalityDotNetCore.Data;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class IntegrationTests
+    {
+        // Arrange
+        private readonly IConfiguration _configuration; // Assurez-vous de fournir une instance valide lors de l'injection
+        private readonly IStringLocalizer<ProductService> _localizer; // Assurez-vous de fournir une instance valide lors de l'injection
+
+        [Fact]
+        public async Task SaveNewProduct()
+        {
+            // Arrange        
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer("Server=localhost;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            using (var ctx = new P3Referential(options, _configuration))
+            {
+                // Injectez les dépendances nécessaires
+                LanguageService languageService = new();
+                Cart cart = new();
+                ProductRepository productRepository = new(ctx);
+                OrderRepository orderRepository = new(ctx);
+                ProductService productService = new(cart, productRepository, orderRepository, _localizer);
+                ProductController productController = new(productService, languageService);
+
+                ProductViewModel productViewModel = new() { Name = "Product from CREATE integration test", Description = "Description", Details = "Detail", Stock = 1, Price = 150 };
+
+                int count = await ctx.Product.CountAsync();
+
+                // Act
+                productController.Create(productViewModel);
+
+                // Assert
+                // Le nombre de produits a augmenté de 1
+                Assert.Equal(count + 1, ctx.Product.Count());
+
+                // Le produit peut être trouvé
+                var product = await ctx.Product.Where(x => x.Name == "Product from CREATE integration test").FirstOrDefaultAsync();
+                Assert.NotNull(product);
+
+                // Nettoyer la BASE DE DONNÉES   
+                ctx.Product.Remove(product);
+                await ctx.SaveChangesAsync();
+            }
+        }
+
+        [Fact]
+        public async Task DeleteProduct()
+        {
+            // Arrange        
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer("Server=localhost;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            using (var ctx = new P3Referential(options, _configuration))
+            {
+                // Injectez les dépendances nécessaires
+                LanguageService languageService = new();
+                Cart cart = new();
+                ProductRepository productRepository = new(ctx);
+                OrderRepository orderRepository = new(ctx);
+                ProductService productService = new(cart, productRepository, orderRepository, _localizer);
+                ProductController productController = new(productService, languageService);
+
+                ProductViewModel productViewModel = new() { Name = "Product from DELETE integration test", Description = "Description", Details = "Detail", Stock = 1, Price = 150 };
+
+                int count = await ctx.Product.CountAsync();
+
+                productController.Create(productViewModel);
+
+                var product = await ctx.Product.Where(x => x.Name == "Product from DELETE integration test").FirstOrDefaultAsync();
+
+                // Act
+                productController.DeleteProduct(product.Id);
+
+                // Assert
+                Assert.Equal(count, ctx.Product.Count());
+
+                var searchProductAgain = await ctx.Product.Where(x => x.Name == "Product from DELETE integration test").FirstOrDefaultAsync();
+                Assert.Null(searchProductAgain);
+            }
+        }
+    }
+}
